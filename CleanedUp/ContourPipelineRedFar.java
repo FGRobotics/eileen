c91339fa43dcd647a490d748a3be80d6ee7a6194
Force Green Robotics 10265 <ForceGreenRobotics@gmail.com>
2024-02-26 22:54:34 -0600

    Update ContourPipelineRedFar.java

--- CleanedUp/ContourPipelineRedFar.java
+++ CleanedUp/ContourPipelineRedFar.java
@@ -77,7 +77,7 @@ public class ContourPipelineRedFar extends OpenCvPipeline {
                     for (int i = (int) topLeft.x; i < (int) bottomRight.x; i++) {
                         for (int j = (int) topLeft.y; j < (int) bottomRight.y; j++) {
 
-                            if (end.get(j, i)[2] > 165) {
+                            if (end.get(j, i)[2] > 120) {
                                 accumulator++;
                             }
                         }
@@ -121,4 +121,3 @@ public class ContourPipelineRedFar extends OpenCvPipeline {
 
 
 }
-
